94409e520ae098123e643026dfbb0af8f9af6104
Yakumo <1668154689@qq.com>
2020-06-23 16:08:27 +0800

    把本地工程添加到Git(master)

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -1,9 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("dev-Hello World");
-        System.out.println("dev-123");
-        System.out.println("dev-hello");
-        System.out.println("dev");
+        System.out.println("hello");
+        System.out.println("123");
+        System.out.println("Hello World");
 
     }
 
