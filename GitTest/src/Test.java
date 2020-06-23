ca36a2eefbbe1c9a8a5c388ac513ae26fe9c7ed9
Yakumo <1668154689@qq.com>
2020-06-23 15:45:01 +0800

    把本地工程添加到Git(dev的第一次修改)

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -1,8 +1,9 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("123");
         System.out.println("Hello World");
+        System.out.println("123");
+        System.out.println("hello");
+
     }
 
     public void Test(){
